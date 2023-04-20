8cddc0693feb2621d6c76eedd4d75cee99e76e79
eduardohpaludo <eduardopaludo@gmail.com>
2023-04-19 22:38:17 -0300

    implement user findByEmail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -22,6 +22,12 @@ export class UserService {
     };
   }
 
+  findByEmail(email: string){
+    this.prisma.user.findUnique({
+      where: { email }
+    })
+  }
+
   findAll() {
     return `This action returns all user`;
   }
