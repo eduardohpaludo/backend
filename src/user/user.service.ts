e75706cc25925a73af83e471f4cab7f2e8660ff0
eduardohpaludo <eduardopaludo@gmail.com>
2023-04-19 22:35:56 -0300

    implement create user with bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10)
+    }
+
+    const createdUser = await this.prisma.user.create({ data })
+    return {
+      ...createdUser,
+      password: undefined
+    };
   }
 
   findAll() {
